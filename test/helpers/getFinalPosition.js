14a38f26620cd8860c651a361edf296468a1faf4
alastair-smith <contact@alsmith.dev>
2023-01-31 13:30:51 +0000

    feat: added basic ci pipeline

---
+++ test/helpers/getFinalPosition.js
@@ -0,0 +1,8 @@
+const { describe, expect, test } = require('@jest/globals')
+const getFinalPosition = require('../../src/helpers/getFinalPosition')
+
+describe('helper getFinalPosition', () => {
+  test('returns true', () => {
+    expect(true).toEqual(true)
+  })
+})
