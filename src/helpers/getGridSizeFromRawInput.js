969f0f955138195ed82ddb7b907aabfcf1732e5e
alastair-smith <contact@alsmith.dev>
2023-01-31 16:10:06 +0000

    feat: get grid size from input

--- src/helpers/getGridSizeFromRawInput.js
+++ src/helpers/getGridSizeFromRawInput.js
@@ -1,1 +1,7 @@
-module.exports = () => {}
+module.exports = rawInput => {
+  if (!/^\d* \d* *$/g.test(rawInput)) throw new Error('Invalid grid size')
+
+  const [xMax, yMax] = rawInput.split(' ').map(Number)
+
+  return { xMax, yMax }
+}
